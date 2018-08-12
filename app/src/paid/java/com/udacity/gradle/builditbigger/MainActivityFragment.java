1db88f05521f058d9856eeb306f3c71c2ee167ce
josephx86 <simplex86@gmail.com>
2018-08-12 02:49:53 -0500

    f

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -18,5 +18,5 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main_activity, container, false);
-    }git stat
+    }
 }
